9f024982b7c5481946fba8a00624ba4bfa72349e
Joel Goncalves <goncalves.joel95@gmail.com>
2025-03-11 19:31:08 -0400

    Deploys flow using serve

--- upload.py
+++ upload.py
@@ -1,27 +1,27 @@
 """Executes flow to upload AudioMoth datasets."""
 
-import asyncio
-
 from flows import upload_datasets
 
 if __name__ == "__main__":
-    asyncio.run(
-        upload_datasets(
+
+    upload_datasets.serve(
+        name="upload-datasets-deployment",
+        parameters={
             # the directory containing the annular eclipse datasets (zipped files)
-            annular_dir="/home/joel/Desktop/zenodo/test/annular",
+            "annular_dir": "/home/joel/Desktop/zenodo/test/annular",
             # the CSV file containing the data collectors information for the annular eclipse data
-            annular_data_collector_csv="/home/joel/Desktop/zenodo/2023_annular_info.csv",
+            "annular_data_collector_csv": "/home/joel/Desktop/zenodo/2023_annular_info.csv",
             # the directory containing the total eclipse datasets (zipped files)
-            total_dir="/home/joel/Desktop/zenodo/test/total",
+            "total_dir": "/home/joel/Desktop/zenodo/test/total",
             # the CSV file containing the data collectors information for the total eclipse data
-            total_data_collector_csv="/home/joel/Desktop/zenodo/2024_total_info.csv",
+            "total_data_collector_csv": "/home/joel/Desktop/zenodo/2024_total_info_updated.csv",
             # a CSV file to save successful results (will be created if does not exist)
-            successful_results_file="/home/joel/Desktop/zenodo/results/successul_results.csv",
+            "successful_results_file": "/home/joel/Desktop/zenodo/results/successul_results.csv",
             # a CSV file to save failed results (will be created if does not exist)
-            failure_results_file="/home/joel/Desktop/zenodo/results/failed_results.csv",
+            "failure_results_file": "/home/joel/Desktop/zenodo/results/failed_results.csv",
             # option to automatically delete any failed uploads
-            delete_failures=True,
+            "delete_failures": True,
             # option to automatically publish a successful upload
-            auto_publish=False,
-        )
+            "auto_publish": False,
+        },
     )
